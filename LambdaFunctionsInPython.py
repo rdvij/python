e432f32aec1f143b074fd5ce64d8dd761769accc
Rajan Deep Vij <rajan@brainstacktechnologies.com>
2025-01-12 19:27:36 +0530

    Changes

--- LambdaFunctionsInPython.py
+++ LambdaFunctionsInPython.py
@@ -41,13 +41,3 @@ from functools import reduce
 numbers = [1, 2, 3, 4, 5]
 sum_of_numbers = reduce(lambda x, y: x + y, numbers)
 print(sum_of_numbers)  # Output: 15
-
-print("----------------------------------------------------------------")
-numbers = [5, 2, 3, 1, 4]
-map = map(lambda x: x**2, numbers)
-while True:
-    try:
-        print(next(map))
-    except StopIteration:
-        break
-print("----------------------------------------------------------------")
