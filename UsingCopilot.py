f58f68fb093e3e179e491000797b6d7d9040cdb6
Rajan Deep Vij <rajan@brainstacktechnologies.com>
2025-01-12 19:22:49 +0530

    Words in file

--- UsingCopilot.py
+++ UsingCopilot.py
@@ -11,3 +11,19 @@ print(strings)
 CapitalizeStrings(strings)
 #
 print("----------------------------------------------------------------")
+
+# This function accepts a list of strings and checks if it can find it in a file.
+def FindStringInFile(strings):
+    with open('sample.txt', 'r') as file:
+        file_content = file.read()
+        for string in strings:
+            if string in file_content:
+                print(f"'{string}' found in the file.")
+            else:
+                print(f"'{string}' not found in the file.")
+
+strings = input("Enter a list of strings separated by a comma: ").split(", ")
+print(strings)
+FindStringInFile(strings)
+#
+print("----------------------------------------------------------------")
