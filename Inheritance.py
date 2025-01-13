acf2a11b24766dc9f9284a2b9cd040d1882fa06e
Rajan Deep Vij <rajan@brainstacktechnologies.com>
2025-01-13 09:02:56 +0530

    Inheritance

---
+++ Inheritance.py
@@ -0,0 +1,64 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print("Name: ", self.name)
+        print("Salary: ", self.salary)
+
+class Manager(Employee):
+    def __init__(self, name, salary, team, *developers):
+        super().__init__(name, salary)
+        self.team = team
+        self.developers = developers
+
+    def display(self):
+        super().display()
+
+        print("Team: ", self.team)
+        print("Developers: ")
+        print("Senior Developers: ", len(list(filter(lambda x: x.type() == "JuniorDeveloper", self.developers))))
+        print("Junior Developers: ", len(list(filter(lambda x: x.type() == "SeniorDeveloper", self.developers))))
+
+class Developer(Employee):
+    def __init__(self, name, salary, project):
+        super().__init__(name, salary)
+        self.project = project
+
+    def display(self):
+        super().display()
+        print("Project: ", self.project)
+
+    def type(self):
+        return type(self).__name__
+    
+class JuniorDeveloper(Developer):
+    def __init__(self, name, salary, project, experience):
+        super().__init__(name, salary, project)
+        self.experience = experience
+
+    def display(self):
+        super().display()
+        print("Experience: ", self.experience)
+
+class SeniorDeveloper(Developer):
+    def __init__(self, name, salary, project, experience):
+        super().__init__(name, salary, project)
+        self.experience = experience
+
+    def display(self):
+        super().display()
+        print("Experience: ", self.experience)
+
+Developer1 = JuniorDeveloper("Raj", 50000, "MFX", 5)
+Developer2 = SeniorDeveloper("Jane", 100000, "MFX", 9)
+
+print(type(Developer1))
+print(type(Developer2))
+print(Developer1.type())
+
+listOfDevelopers = [Developer1, Developer2]
+Manager1 = Manager("John", 90000, "MFX", Developer1, Developer2)
+
+Manager1.display()
